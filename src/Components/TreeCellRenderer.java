a759790f08a51f2b7b0f01be6ce01702aa995217
luke <50461140+0luke@users.noreply.github.com>
2024-02-02 12:21:08 +0000

    made it green upon selection

--- src/Components/TreeCellRenderer.java
+++ src/Components/TreeCellRenderer.java
@@ -15,7 +15,7 @@ public class TreeCellRenderer extends DefaultTreeCellRenderer{
 
 	@Override
 	public Component getTreeCellRendererComponent(JTree tree, Object value,
-			boolean sel, boolean expanded, boolean leaf, int row,
+			boolean selected, boolean expanded, boolean leaf, int row,
 			boolean hasFocus) {
 
 		Component c = super.getTreeCellRendererComponent(tree, value,
@@ -30,6 +30,9 @@ public class TreeCellRenderer extends DefaultTreeCellRenderer{
 		
 		setIcon(ImageLoader.loadImage("images/document_small.png"));
 		
+		if(selected) {
+			setBackgroundSelectionColor(Color.GREEN);
+		}
 		
 		return c;
 	}
